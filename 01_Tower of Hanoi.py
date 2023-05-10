00a1600b049f9daa00db5c1330bfc5555b6f040f
Pritirekha23 <learnpriti23@gmail.com>
2023-05-10 13:05:23 +0530

    Adding Tower of Hanoi problem solution

---
+++ 01_Tower of Hanoi.py
@@ -0,0 +1,24 @@
+'''TOWER OF HANOI'''
+
+
+def tower_of_hanoi(disk, source, helper, destination):
+    if disk == 1:
+        print(f'Move disk 1 from tower {source} to tower {destination}')
+        return
+    tower_of_hanoi(disk-1, source, destination, helper)
+    print(f'Move disk  {disk} from tower  {source} to tower {destination}')
+    tower_of_hanoi(disk-1, helper, source, destination)
+
+disk=int(input('Enter the number of disks::'))
+tower_of_hanoi(3, 'A', 'B', 'C')
+
+'''
+output:
+Enter the number of disks::3
+Move disk 1 from tower A to tower C
+Move disk  2 from tower  A to tower B
+Move disk 1 from tower C to tower B
+Move disk  3 from tower  A to tower C
+Move disk 1 from tower B to tower A
+Move disk  2 from tower  B to tower C
+Move disk 1 from tower A to tower C  '''
