c415033b6a8ca3c27483851af9f4aae47a66675b
Pritirekha23 <learnpriti23@gmail.com>
2023-05-12 23:28:33 +0530

    adding stack implementation using list

--- Stack/01_stack_implementation_using_list.py
+++ Stack/01_stack_implementation_using_list.py
@@ -8,35 +8,322 @@
      isEmpty()-Check whether any element are stored in the stack or not  so if the stack is empty then pop() and top() operation can not be performed that is called as UNDERFLOW.
      isFull()-It Checks whether the stack is full or not , if the stack is full then push() operation cant be perfomed that is called as OVERFLOW.'''
      
-#We can implent stack using list and modules.
+#We can implement stack using list and modules.
 #STACK IMPLEMETATION USING LIST    
  
 stack=[]
 
 def push():
-    data=input('Enter the data::')
-    stack.append(data)
-    print('Present elements in the stack are:',stack)
+    if len(stack)==n:
+        print('stack is full')
+    else:
+        data=input('Enter the data::')
+        stack.append(data)
+        
     
 def pop():
+    if  stack==[]:
+        print('Stack is Empty')
+    else:
+        print('Element deleted from the top of the stack')
+        a=stack.pop() 
+        
+ 
+def display():
+    print('Present elements in the stack are:',stack)
+     
+def peek():
     if stack==[]:
         print('Stack is Empty')
     else:
-        a=stack.pop()
-        print('Element deleted from the top of the stack')
-        print('Present elements in the stack are:',stack)
-        
+        print(stack[-1])
+
+    
+n=int(input('Limit of stack::'))        
 while True:
-    print('Select the operations :: \n1.push \n 2.pop\n 3.Exit\n ')
-    choice=int(input('Select any one of the above operations::'))
+    print('Select the operations :: \n1.push \n 2.pop\n 3.display\n 4.peek\n 5.Exit\n ')
+    choice=int(input('Choose any one of the above operations::'))
     if choice==1:
         push()
     elif choice==2:
         pop()
     elif choice==3:
+        display()
+    elif choice==4:
+        peek()
+        
+    elif choice==5:
         break
+    
     else:
-        print('Invalid choice please choose a valid number from 1,2 or 3')
+        print('Invalid choice please choose a valid number ')
+ 
+
+'''output:
+Limit of stack::4
+Select the operations :: 
+1.push 
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+ 
+Choose any one of the above operations::1
+Enter the data::30
+Select the operations :: 
+1.push 
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+ 
+Choose any one of the above operations::2
+Element deleted from the top of the stack
+Select the operations :: 
+1.push 
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+ 
+Choose any one of the above operations::4
+Stack is Empty
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+Enter the data::priti
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+Enter the data::45
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+Enter the data::0.3
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+Enter the data::65
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+stack is full
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::544
+Invalid choice please choose a valid number
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+stack is full
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::3
+Present elements in the stack are: ['priti', '45', '0.3', '65']
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::1
+stack is full
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::4
+65
+Select the operations ::
+1.push
+ 2.pop
+ 3.display
+ 4.peek
+ 5.Exit
+
+Choose any one of the above operations::5
+   '''        
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# stack=[]
+
+# def push():
+#     data=input('Enter the data::')
+#     stack.append(data)
+#     print('Present elements in the stack are:',stack)
+    
+# def pop():
+#     if stack==[]:
+#         print('Stack is Empty')
+#     else:
+#         a=stack.pop()
+#         print('Element deleted from the top of the stack')
+#         print('Present elements in the stack are:',stack)
+        
+# while True:
+#     print('Select the operations :: \n1.push \n 2.pop\n 3.Exit\n ')
+#     choice=int(input('Select any one of the above operations::'))
+#     if choice==1:
+#         push()
+#     elif choice==2:
+#         pop()
+#     elif choice==3:
+#         break
+#     else:
+#         print('Invalid choice please choose a valid number from 1,2 or 3')
     
     
     
