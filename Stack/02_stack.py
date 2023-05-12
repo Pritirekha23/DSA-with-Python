c3fd34836e8a1708e522eb02563cd9739e8baead
Pritirekha23 <learnpriti23@gmail.com>
2023-05-12 22:27:24 +0530

    adding stack implementation using list

--- Stack/02_stack.py
+++ Stack/02_stack.py
@@ -12,14 +12,15 @@ def pop():
     if not stack:
         print('Stack is Empty')
     else:
-        a=stack.pop()
         print('Element deleted from the top of the stack')
+        a=stack.pop() 
         print('Present elements in the stack are:',stack)
+ 
  
 n=int(input('Limit of stack::'))        
 while True:
     print('Select the operations :: \n1.push \n 2.pop\n 3.Exit\n ')
-    choice=int(input('Select any one of the above operations::'))
+    choice=int(input('Choose any one of the above operations::'))
     if choice==1:
         push()
     elif choice==2:
@@ -32,89 +33,98 @@ while True:
         
         
         '''OUTPUT::
-        Limit of stack::4
+        Limit of stack::5
 Select the operations :: 
 1.push 
  2.pop
  3.Exit
  
-Select any one of the above operations::1
-Enter the data::20
-Present elements in the stack are: ['20']
+Choose any one of the above operations::1
+Enter the data::10
+Present elements in the stack are: ['10']
 Select the operations :: 
 1.push 
  2.pop
  3.Exit
  
-Select any one of the above operations::1
-Enter the data::30
-Present elements in the stack are: ['20', '30']
+Choose any one of the above operations::1
+Enter the data::'RAm'
+Present elements in the stack are: ['10', "'RAm'"]
 Select the operations :: 
 1.push 
  2.pop
  3.Exit
  
-Select any one of the above operations::1
-Enter the data::40
-Present elements in the stack are: ['20', '30', '40']
+Choose any one of the above operations::1
+Enter the data::0.2
+Present elements in the stack are: ['10', "'RAm'", '0.2']
 Select the operations :: 
 1.push 
  2.pop
  3.Exit
  
-Select any one of the above operations::1     
-Enter the data::50
-Present elements in the stack are: ['20', '30', '40', '50']
+Choose any one of the above operations::1     
+Enter the data::233
+Present elements in the stack are: ['10', "'RAm'", '0.2', '233']
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::1     
+Choose any one of the above operations::1     
+Enter the data::24
+Present elements in the stack are: ['10', "'RAm'", '0.2', '233', '24']
+Select the operations ::
+1.push
+ 2.pop
+ 3.Exit
+
+Choose any one of the above operations::1     
 stack is full
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::2     
+Choose any one of the above operations::2     
 Element deleted from the top of the stack     
-Present elements in the stack are: ['20', '30', '40']
+Present elements in the stack are: ['10', "'RAm'", '0.2', '233']
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::2     
+Choose any one of the above operations::2     
 Element deleted from the top of the stack     
-Present elements in the stack are: ['20', '30']
+Present elements in the stack are: ['10', "'RAm'", '0.2']
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::2     
+Choose any one of the above operations::2     
 Element deleted from the top of the stack     
-Present elements in the stack are: ['20']     
+Present elements in the stack are: ['10', "'RAm'"]
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::2     
+Choose any one of the above operations::2     
 Element deleted from the top of the stack     
-Present elements in the stack are: []
+Present elements in the stack are: ['10']     
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::2     
-Stack is Empty
+Choose any one of the above operations::34    
+Invalid choice please choose a valid number from 1,2 or 3
 Select the operations ::
 1.push
  2.pop
  3.Exit
 
-Select any one of the above operations::3  '''
+Choose any one of the above operations::3
+          '''
     
