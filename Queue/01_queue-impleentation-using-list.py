65d7393a563d46c5f3d7f2cd763709c9bdc862d7
Pritirekha23 <learnpriti23@gmail.com>
2023-05-14 11:12:21 +0530

    Day 76 of 100Days of Code

--- Queue/01_queue-impleentation-using-list.py
+++ Queue/01_queue-impleentation-using-list.py
@@ -18,7 +18,7 @@ def deQueue():
         print(f'{e} is removed from the Queue')
         
         
-def peek():
+def front():
     if Q==[]:
         print('Queue is Empty')
     else:
@@ -29,14 +29,14 @@ def display():
     
 n=int(input('Limit of Queue:'))     
 while True:
-    print('Select the operation \n 1.enqueue\n 2.deQueue\n 3.peek\n 4.display\n 5.Exit')
+    print('Select the operation \n 1.enqueue\n 2.deQueue\n 3.front\n 4.display\n 5.Exit')
     choice=int(input('Choose any one from the above:'))
     if choice==1:
         enQueue()
     elif choice==2:
         deQueue()
     elif choice==3:
-        peek()
+        front()
     elif choice==4:
         display()
     elif choice==5:
@@ -45,55 +45,130 @@ while True:
         print('Invalid choice please choose a valid option')
         
         
-'''output:
+'''output
 Limit of Queue:3
 Select the operation 
  1.enqueue
  2.deQueue
- 3.peek
+ 3.front
  4.display
  5.Exit
 Choose any one from the above:1
-Enter the Element:23
-23 is added to the Queue
+Enter the Element:27
+27 is added to the Queue
 Select the operation 
  1.enqueue
  2.deQueue
- 3.peek
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:1
+Enter the Element:356
+356 is added to the Queue
+Select the operation 
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:1
+Enter the Element:53
+53 is added to the Queue
+Select the operation 
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:1
+Queue is full
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:3
+27
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:2
+27 is removed from the Queue
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:2
+356 is removed from the Queue
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:2
+53 is removed from the Queue
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:2
+Queue is empty:
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:3
+Queue is Empty
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
+ 4.display
+ 5.Exit
+Choose any one from the above:1
+Enter the Element:45
+45 is added to the Queue
+Select the operation
+ 1.enqueue
+ 2.deQueue
+ 3.front
  4.display
  5.Exit
 Choose any one from the above:1
 Enter the Element:43
 43 is added to the Queue
-Select the operation 
+Select the operation
  1.enqueue
  2.deQueue
- 3.peek
+ 3.front
  4.display
  5.Exit
 Choose any one from the above:1
-Enter the Element:54
-54 is added to the Queue
-Select the operation 
- 1.enqueue
- 2.deQueue
- 3.peek
- 4.display
- 5.Exit
-Choose any one from the above:3 
-23
+Enter the Element:53
+53 is added to the Queue
 Select the operation
  1.enqueue
  2.deQueue
- 3.peek
+ 3.front
  4.display
  5.Exit
 Choose any one from the above:4
-Elements present in the Queue are: [23, 43, 54]
+Elements present in the Queue are: [45, 43, 53]
 Select the operation
  1.enqueue
  2.deQueue
- 3.peek
+ 3.front
  4.display
  5.Exit
 Choose any one from the above:5'''
